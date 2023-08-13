66dea2682adf685208285f866f1ef0f9fa4eeacb
hosseinemir <hosseinemir79@gmail.com>
2023-08-13 18:36:47 +0330

    finish menu page styles

--- components/modules/Card.js
+++ components/modules/Card.js
@@ -6,7 +6,7 @@ import Dollar from "../icons/Dollar";
 export default function Card(props) {
   const { id, name, price, discount, details } = props;
   return (
-    <div>
+    <div className={styles.container}>
       <img src={`/images/${id}.jpeg`} alt={name} />
       <div className={styles.name}>
         <p>{name}</p>
@@ -18,13 +18,19 @@ export default function Card(props) {
       <div className={styles.price}>
         <Dollar />
         {discount ? (
-          <p className={styles.discount}>{(price * (100 - discount)) / 100}$</p>
+          <p className={styles.discount}>
+            {(price * (100 - discount)) / 100} $
+          </p>
         ) : (
-          <p>{price}</p>
+          <p>{price} $</p>
         )}
       </div>
       {discount ? <div className={styles.badge}>{discount} %</div> : null}
-      <Link href={`/menu/${id}`}>see details</Link>
+      <div className={styles.linkcontainer}>
+        <Link className={styles.link} href={`/menu/${id}`}>
+          see details
+        </Link>
+      </div>
     </div>
   );
 }
