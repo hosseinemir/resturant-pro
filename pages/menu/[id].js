3a96e4a749a044ebb5a385337addb9f9df4c3114
hosseinemir <hosseinemir79@gmail.com>
2023-08-14 00:24:21 +0330

    make food detaild page completed

---
+++ pages/menu/[id].js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router"
+import DetailsPage from "@/components/template/DetailsPage"
+
+export default function Details({data}) {
+  const router=useRouter()
+  if (router.isFallback){
+    return <h2>loading</h2>
+    
+  }
+
+    return (
+    <div><DetailsPage {...data}/></div>
+  )
+}
+
+export async function getStaticPaths(){
+    const res= await fetch('http://localhost:3001/data')
+    const json = await res.json()
+    const data = json.slice(0,10)
+    const paths = data.map(item=>({params:{id: item.id.toString()}}))
+
+    return{
+        paths,
+        fallback:true,
+    }
+}
+
+export async function getStaticProps(context){
+    const {params:{id}}=context;
+    const res = await fetch(`http://localhost:3001/data/${id}`)
+    const data = await res.json();
+    if (!data.id){
+        return{
+            notFound:true
+        }
+    }
+    return{
+        props:{data,},
+        revalidate:10,
+    }
+}
